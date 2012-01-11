3da91b0544d88541277317abb1ce264bd240f7ee
phillc <spyyderz@gmail.com>
2012-01-11 17:20:45 -0500

    More helpers

--- app/helpers/vehicles_helper.rb
+++ app/helpers/vehicles_helper.rb
@@ -2,4 +2,16 @@ module VehiclesHelper
   def my_form action
     "<form action=\"#{action}\" method=\"post\">"
   end
+  
+  def my_text_field name, options={}
+    "<input id=\"#{options[:id]}\" name=\"#{name}\" size=\"30\" type=\"text\" />"
+  end
+
+  def my_label name, content
+    "<label for=\"#{name}\">#{content}</label>"
+  end
+  
+  def my_submit value, options={}
+    "<input id=\"#{options[:id]}\" name=\"commit\" type=\"submit\" value=\"#{value}\" />"
+  end
 end
